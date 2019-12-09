5960d755748fa6280c32a59178921c747d15bde1
Mircea Nedelcu <mnedelcu@cj.com>
2019-12-08 16:56:22 -0800

    move list of users in index.js

--- api/src/index.js
+++ api/src/index.js
@@ -1,5 +1,4 @@
 import express from 'express';
-import db from './db';
 import bodyParser from 'body-parser';
 
 const app = express();
@@ -7,10 +6,26 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+const users =  [
+    {
+        id: 1,
+        name: "Alice",
+        birthDate: "2010-01-01"
+    }, {
+        id: 2,
+        title: "Bob",
+        birthDate: "2005-12-12"
+    }, {
+        id: 3,
+        title: "Mike",
+        birthDate: "2000-12-12"
+    }
+];
+
 app.get('/api/v1/users', (req, res) => {
     res.status(200).send({
         success: 'true',
-        todos: db
+        todos: users
     })
 });
 
@@ -22,11 +37,11 @@ app.post('/api/v1/users', (req, res) => {
         });
     }
     const user = {
-        id: db.length + 1,
+        id: users.length + 1,
         name: req.body.name,
         birthDate: req.body.birthDate
     };
-    db.push(user);
+    users.push(user);
     return res.status(201).send({
         success: 'true',
         message: 'user added successfully',
