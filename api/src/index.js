a580f8db3e584e52acb997a7e75fed662c153335
Mircea Nedelcu <mnedelcu@cj.com>
2019-12-08 16:57:32 -0800

    make id string

--- api/src/index.js
+++ api/src/index.js
@@ -8,17 +8,13 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 const users =  [
     {
-        id: 1,
+        id: "0",
         name: "Alice",
         birthDate: "2010-01-01"
     }, {
-        id: 2,
+        id: "1",
         title: "Bob",
         birthDate: "2005-12-12"
-    }, {
-        id: 3,
-        title: "Mike",
-        birthDate: "2000-12-12"
     }
 ];
 
